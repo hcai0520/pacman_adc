fe9f699d4d1df320354ad22a2df7b208bc371434
Hong Cai <hongc@Hongs-MacBook-Pro.local>
2025-10-06 16:37:46 -0700

    test adc_ bram

--- ghdl/adc/adc_bram.vhd
+++ ghdl/adc/adc_bram.vhd
@@ -64,8 +64,8 @@ begin
   BRAM_DATA_O <= data;
   BRAM_WEN_O  <= wen;
 
-  buff_size   <= CONFIG_I(10 downto  0);
-  packing     <= CONFIG_I(15 downto 12);
+  buff_size   <= CONFIG_I;
+  --packing     <= CONFIG_I(15 downto 12);
 
   STATUS_O    <= stat;
   LAST_O      <= last;
@@ -84,6 +84,7 @@ begin
       end if;        
     end if;
   end process;
+
   process(clk,rst)
   begin
     if (rst = '1') then
@@ -91,76 +92,34 @@ begin
       data     <= (others => '0');
       addr     <= (others => '0');
     elsif (rising_edge(clk)) then
---     if (packing = x"0" or valid = '0') then -- reset state
-      if (packing = x"0" ) then -- reset state
-        wen      <= (others => '0');
-        data     <= (others => '0');
-        addr     <= (others => '0');
-      elsif (packing = x"1") then
-        data(12 downto 0)  <= data_in;
-        data(31 downto 13) <= (others => '0');
-        wen                <= (others => '1');
-        if unsigned(buff_size) /= 0 and unsigned(addr) >= unsigned(buff_size) then
-          addr <= (others => '0');
-        else
-          addr <= std_logic_vector(unsigned(addr) + 1);
-        end if;
-      elsif (packing = x"2") then -- bit packing (no loss of data)
-        data(12 downto 0)  <= data_in;
-        data(15 downto 13) <= (others => '0');
-        data(28 downto 16) <= data_in;
-        data(31 downto 29)  <= (others => '0');
-        if (wen = x"3") then
-          wen <= x"C";
-        else
-          wen <= x"3";
-          if unsigned(buff_size) /= 0 and unsigned(addr) >= unsigned(buff_size) then
-            addr <= (others => '0');
-          else
-            addr <= std_logic_vector(unsigned(addr) + 1);
-          end if;
-        end if;
-      elsif (packing = x"3") then --very packed (loss of 4 bits of data + overflow)
-        data(7  downto  0) <= data_in(11 downto 4);
-        data(15 downto  8) <= data_in(11 downto 4);
-        data(23 downto 16) <= data_in(11 downto 4);
-        data(31 downto 24) <= data_in(11 downto 4);
-        if (wen = x"1") then
-          wen <= x"2";
-        elsif (wen = x"2") then
-          wen <= x"4";
-        elsif (wen = x"4") then
-          wen <= x"8";
-        else
-          wen <= x"1";
-          if unsigned(buff_size) /= 0 and unsigned(addr) >= unsigned(buff_size) then
-            addr <= (others => '0');
-          else
-            addr <= std_logic_vector(unsigned(addr) + 1);
-          end if;
-        end if;
+      data(12 downto 0)  <= data_in;
+      data(31 downto 13) <= (others => '0');
+      wen                <= x"F";
+      if (unsigned(buff_size) = 0) then
+        addr <= std_logic_vector(unsigned(addr) + 1);
+      elsif (unsigned(addr) >= unsigned(buff_size)) then
+        addr <= (others => '0');
       else
-        wen      <= (others => '0');
-        data     <= (others => '0');
-        addr     <= (others => '0');
+        addr <= std_logic_vector(unsigned(addr) + 1);
       end if;
     end if;
   end process;
 
-  process(clk,rst) -- gets status and last
-  begin
-    if (rst = '1') then
-      last <= (others => '0');
-      stat <= (others => '0');
-    else
-      if (rising_edge(clk)) then
-        if (wen /= x"0") then
-          stat(10 downto 0) <= addr;
-          stat(15 downto 12) <= wen;
-          last <= data;
-        end if;
-      end if;
-    end if;
-  end process;
+
+  --process(clk,rst) -- gets status and last
+  --begin
+  --  if (rst = '1') then
+  --    last <= (others => '0');
+  --    stat <= (others => '0');
+  --  else
+  --    if (rising_edge(clk)) then
+  --      if (wen /= x"0") then
+  --         stat(10 downto 0) <= addr;
+  --        stat(15 downto 12) <= wen;
+  --        last <= data;
+  --      end if;
+  --    end if;
+  --  end if;
+  --end process;
 
 end behavioral;
